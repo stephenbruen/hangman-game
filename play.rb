80329b18dfc83c7b891884d11652b945d0f9a8c6
Stephen H Bruen <52644685+stephenbruen@users.noreply.github.com>
2023-03-09 23:34:12 -0800

    Add files via upload

---
+++ play.rb
@@ -0,0 +1,16 @@
+class Hangman
+
+        def initialize
+            letters = ('a'..'z').to_a
+            word = words.sample
+        end
+        def words
+            [
+                "cricket", 
+            ]
+
+
+        def begin
+            # ask user for a letter
+        end
+    end
